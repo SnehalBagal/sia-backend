fae2645f1de3ab8a4067d1c306cd9299b47faa0f
Sneha Bagal <b09sneha@gmail.com>
2026-05-29 08:29:07 +0530

    Fixed Railway DB config

--- app/database/db.py
+++ app/database/db.py
@@ -3,11 +3,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-MYSQLHOST = os.getenv("mysql.railway.internal")
-MYSQLPORT = os.getenv("3306")
-MYSQLUSER = os.getenv("root")
-MYSQLPASSWORD = os.getenv("PEbUCyqyqXvXCMRiupKpEJfpdtlrFAzv")
-MYSQLDATABASE = os.getenv("railway")
+MYSQLHOST = "mysql.railway.internal"
+MYSQLPORT = "3306"
+MYSQLUSER = "root"
+MYSQLPASSWORD = "PEbUCyqyqXvXCMRiupKpEJfpdtlrFAzv"
+MYSQLDATABASE = "railway"
 
 DATABASE_URL = (
     f"mysql+pymysql://{MYSQLUSER}:{MYSQLPASSWORD}"
