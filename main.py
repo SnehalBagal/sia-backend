2a80f942d39fe7321b5d54769e32a90724e63dda
Sneha Bagal <b09sneha@gmail.com>
2026-06-14 08:24:21 +0530

    Add SIA domain to CORS

--- main.py
+++ main.py
@@ -44,10 +44,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_origins=[
+        "https://sia.kpaindia.co.in",
+        "https://sia-frontend-lemon.vercel.app",
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
     allow_headers=["*"],
+)
 )
 
 def get_db():
