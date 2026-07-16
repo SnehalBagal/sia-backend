749e4e5d6a1f3e4d663e838999d37662030ccc33
SnehalBagal <100023309+SnehalBagal@users.noreply.github.com>
2026-07-16 16:46:19 +0530

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -3,18 +3,21 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-MYSQLHOST = "mysql.railway.internal"
-MYSQLPORT = 3306
-MYSQLUSER = "root"
-MYSQLPASSWORD = "PEbUCyqyqXvXCMRiupKpEJfpdtlrFAzv"
-MYSQLDATABASE = "railway"
+MYSQLHOST = os.getenv("MYSQLHOST")
+MYSQLPORT = os.getenv("MYSQLPORT")
+MYSQLUSER = os.getenv("MYSQLUSER")
+MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
+MYSQLDATABASE = os.getenv("MYSQLDATABASE")
 
 DATABASE_URL = (
     f"mysql+pymysql://{MYSQLUSER}:{MYSQLPASSWORD}"
     f"@{MYSQLHOST}:{MYSQLPORT}/{MYSQLDATABASE}"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
